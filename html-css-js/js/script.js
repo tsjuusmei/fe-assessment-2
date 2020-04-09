c4be3bf23638fed9c98df351d0501c0970c73960
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 14:22:50 +0200

    made profile name dynamic with js

--- html-css-js/js/script.js
+++ html-css-js/js/script.js
@@ -7,10 +7,11 @@ let currentUser;
 axios.get('https://fe-assessment-2-be.herokuapp.com/users')
     .then(response => {
         users = response.data;
-
         axios.get('https://fe-assessment-2-be.herokuapp.com/userID')
             .then(response => {
                 userID = response.data;
                 currentUser = users.filter(data => { return data.id == userID })[0]
             })
     })
+
+document.querySelector(".name").innerHTML = currentUser.name
