1c3f85981adf75f86a8e5cf4c0b74487a28ce880
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 14:14:56 +0200

    fixed currentUser

--- html-css-js/js/script.js
+++ html-css-js/js/script.js
@@ -2,16 +2,15 @@ let users = [];
 
 let userID;
 
+let currentUser;
+
 axios.get('https://fe-assessment-2-be.herokuapp.com/users')
-.then(response => {
-  users = response.data;
-})
+    .then(response => {
+        users = response.data;
 
-axios.get('https://fe-assessment-2-be.herokuapp.com/userID')
-.then(response => {
-    userID = response.data;
-})
-
-const currentUser = users.filter(data => { return data.id == userID })[0];
-
-console.log(currentUser)
+        axios.get('https://fe-assessment-2-be.herokuapp.com/userID')
+            .then(response => {
+                userID = response.data;
+                currentUser = users.filter(data => { return data.id == userID })[0]
+            })
+    })
