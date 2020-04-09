fc78bdb814a2a2d3072fa456886060f97ec9dc0c
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 15:03:17 +0200

    making promises list for visitors

--- html-css-js/js/script.js
+++ html-css-js/js/script.js
@@ -13,10 +13,14 @@ axios.get('https://fe-assessment-2-be.herokuapp.com/users')
                 currentUser = users.filter(data => { return data.id == userID })[0]
 
                 document.querySelector(".name").innerHTML = currentUser.name
-
                 document.querySelector(".numberVisitors").innerHTML = currentUser.visitedBy.length
-
                 document.querySelector(".numberLikes").innerHTML = currentUser.likedBy.length
+
+                const promises = [];
+                currentUser.visitedBy.forEach(visitorID => {
+                    promises.push(users.findOne({ id: visitorID}))
+                });
+                
             })
     })
 
