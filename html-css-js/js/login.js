ce4f18bb7404aa5c810b5ae232d3f19be77a9d55
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 11:21:06 +0200

    fixed login

--- html-css-js/js/login.js
+++ html-css-js/js/login.js
@@ -4,15 +4,11 @@ let users = [];
 
 axios.get('https://fe-assessment-2-be.herokuapp.com/users')
 .then(response => {
-  users = JSON.parse(response.data);
+  users = response.data;
   userDropdown()
 })
 
 function userDropdown() {
-
-for (let i=0; users.length > i; i++) {
-  users.push(users[i])
-  }
   users.forEach(user => {
   const option = document.createElement('OPTION');
   option.setAttribute('value', user.id);
