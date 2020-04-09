5c5e1806208b7bad62bde442530f91933f6622cf
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 14:24:38 +0200

    moved line in promise

--- html-css-js/js/script.js
+++ html-css-js/js/script.js
@@ -11,7 +11,7 @@ axios.get('https://fe-assessment-2-be.herokuapp.com/users')
             .then(response => {
                 userID = response.data;
                 currentUser = users.filter(data => { return data.id == userID })[0]
+                document.querySelector(".name").innerHTML = currentUser.name
             })
     })
 
-document.querySelector(".name").innerHTML = currentUser.name
