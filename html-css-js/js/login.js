3126e34b140417d14e3d5e56533bc74040ff2b95
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 12:08:58 +0200

    added currentuser variable

--- html-css-js/js/login.js
+++ html-css-js/js/login.js
@@ -2,6 +2,8 @@ const userSelect = document.getElementById('user');
 
 let users = [];
 
+let currentUser;
+
 axios.get('https://fe-assessment-2-be.herokuapp.com/users')
 .then(response => {
   users = response.data;
--- html-css-js/js/script.js
+++ html-css-js/js/script.js
@@ -1,3 +1,13 @@
-let users;
+let users = [];
 
 let currentUser;
+
+axios.get('https://fe-assessment-2-be.herokuapp.com/users')
+.then(response => {
+  users = response.data;
+})
+
+axios.get('https://fe-assessment-2-be.herokuapp.com/currentUser')
+.then(response => {
+    currentUser = response.data;
+})
