549db8413bb1bcaa9845ab34feb280d5a5e37e6a
tsjuusmei <guusmay2@hotmail.com>
2020-04-09 14:35:51 +0200

    amounts made dynamic

--- html-css-js/js/script.js
+++ html-css-js/js/script.js
@@ -11,7 +11,12 @@ axios.get('https://fe-assessment-2-be.herokuapp.com/users')
             .then(response => {
                 userID = response.data;
                 currentUser = users.filter(data => { return data.id == userID })[0]
+
                 document.querySelector(".name").innerHTML = currentUser.name
+
+                document.querySelector(".numberVisitors").innerHTML = currentUser.visitedBy.length
+
+                document.querySelector(".numberLikes").innerHTML = currentUser.likedBy.length
             })
     })
 
